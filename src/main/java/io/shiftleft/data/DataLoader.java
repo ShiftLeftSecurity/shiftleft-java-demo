64e1c0670ac7336ff1cf690432f0b81d52cff666
Chetan Conikee <conikee@gmail.com>
2023-10-14 17:19:01 -0700

    logging access details

--- src/main/java/io/shiftleft/data/DataLoader.java
+++ src/main/java/io/shiftleft/data/DataLoader.java
@@ -61,7 +61,6 @@ public class DataLoader implements CommandLineRunner {
     log.info("AWS AccessKey is {} and SecretKey is {}", env.getProperty("aws.accesskey"),
         env.getProperty("aws.secretkey"));
     log.info("AWS Bucket is {}", env.getProperty("aws.bucket"));
-    log.info("End Loading AWS Properties");
 
     // Connect to AWS resources and do something
 
